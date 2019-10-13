cf65762a23843bbf64d34f69030d29a4ef62dec3
fanzhangg <vanadiumzhang@gmail.com>
2019-10-12 22:12:27 -0500

    - ADD: click a key to trigger the key sound - ADD: highlight the keys on the piano after pressing a key - FIX: click the text on the key will trigger the sound - FIX: the keys of the first 2 octave will now display properly in the full-feature piano mode - CHANGE: the key starts from C1 - CHANGE: refract the code, and put note-related methods in the note manager

--- app/src/keyboard/Keyboard.mjs
+++ app/src/keyboard/Keyboard.mjs
@@ -1,12 +1,12 @@
 import {KeyboardElement} from "./Element.mjs";
+import {NoteManager} from "./NoteManager.mjs";
 
 class Keyboard {
     constructor(container){
         this._container = container;
-        this.maxId = 0;
 
         // The piano keyboard interface
-        this._keyboardInterface = new KeyboardElement(container, 48, 2);
+        this._keyboardInterface = new KeyboardElement(container);
         window.addEventListener('resize', this._resize.bind(this)); // Resize the keyboard according to the width of the window
         this._resize();
         this._bindSound();
@@ -17,25 +17,10 @@ class Keyboard {
         let octaves = Math.round((window.innerWidth / keyWidth) / 12);
         octaves = Math.max(octaves, 2);
         octaves = Math.min(octaves, 7);
-        let baseNote = 0;
-        if (octaves > 5){
-            baseNote -= (octaves - 5) * 12;
-        }
-        this.maxId = baseNote + octaves * 12 - 1;
-        this._keyboardInterface.resize(baseNote, octaves);  // Populate keys
+        this._keyboardInterface.resize(0, octaves);  // Populate keys
     }
 
     _bindSound(){
-        // let samples = {};
-        //
-        // for (let i = 1; i < 8; i++){
-        //     for (let j = 0; j < 3; j ++){
-        //         const octave = i.toString();
-        //         const note = String.fromCharCode('A'.charCodeAt(0) + j);
-        //         const pitch = `${note}${octave}`.toString();
-        //         samples[pitch] = `${pitch}.mp3`;
-        //     }
-        // }
         // bind each key to trigger corresponding sound
         const sampler = new Tone.Sampler({
             "A0" : "A0.[mp3|ogg]",
@@ -75,27 +60,17 @@ class Keyboard {
                 document.addEventListener('keydown', e => {
                     // Press a key on keyboard to trigger the corresponding sound
                     const noteId = "1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./".indexOf(e.key);  // Get the note id bind by the key
-                    let note = noteId % 12;
-                    const chromatic = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
-                    note = chromatic[note];
-                    const octave = Math.floor(noteId / 12) + 2;
-                    console.log(`${note}${octave}`);
-                    sampler.triggerAttackRelease(`${note}${octave}`, "4n");
+                    const note = NoteManager.getNote(noteId);
+                    sampler.triggerAttackRelease(note, "4n");
                 });
 
-                for(let i = 0; i < 59; i++){
-                    const key = document.getElementById(i.toString());
-                    key.addEventListener('pointerup', e=>{
-                        //TODO: pressing the last key does not trigger a sound
-                        let note = i % 12;
-                        const chromatic = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
-                        note = chromatic[note];
-                        const octave = Math.floor(i / 12) + 2;
-
-                        sampler.triggerAttackRelease(`${note}${octave}`, "4n");
-
-                    })
-                }
+                document.addEventListener('pointerdown', (e)=>{
+                    const id = e.target.id;
+                    if (id !== "") {
+                        const note = NoteManager.getNote(e.target.id);
+                        sampler.triggerAttackRelease(note, "4n");
+                    }
+                })
             }
         }).toMaster();
     }
