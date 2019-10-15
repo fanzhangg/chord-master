98f422c0096414f8baf41eec038a812dec30fbda
fanzhangg <vanadiumzhang@gmail.com>
2019-10-15 15:09:00 -0500

    Refactoring, added key bindings

--- app/src/keyboard/Element.mjs
+++ app/src/keyboard/Element.mjs
@@ -1,3 +1,5 @@
+import {NoteManager} from "./NoteManager.mjs"
+
 const offsets = [0, 0.5, 1, 1.5, 2, 3, 3.5, 4, 4.5, 5, 5.5, 6];
 
 const chords = {
@@ -7,19 +9,18 @@ const chords = {
 };
 
 class KeyboardElement{
-    constructor(container, lowest=36, octaves=4){
+    constructor(container){
         this._container = document.createElement('div');
         this._container.id = 'keyboard';
         container.setAttribute('touch-action', 'none');
         container.appendChild(this._container);
 
         this._keys = {};
-        this.resize(lowest, octaves);
 
         this._prevKeys = [];
     }
 
-    resize(lowest, octaves){
+    resize(lowest=0, octaves){
         this._keys = {};
         // clear the previous ones
         this._container.innerHTML = '';
@@ -35,20 +36,24 @@ class KeyboardElement{
             }
 
             // Add the note on the key if it is C
-            const note = this._getNote(i);
+            const note = NoteManager.getNote(i);
             if (note.search(/C[^#]/) === 0){  // Starts with C, not C#
                 const noteStr = document.createElement("p");
-                noteStr.id = 'noteStr';
+                noteStr.classList.add('noteStr');
+                noteStr.id = i.toString();
                 noteStr.innerText = note;
                 key.appendChild(noteStr);
             }
 
-            // Add the key binding to the piano key
+            // Add the key text in the div
             const keyStr = document.createElement("p");
-            keyStr.id = 'keyStr';
-            keyStr.innerText = "1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./".charAt(i);
+            keyStr.classList.add('keyStr');
+            keyStr.id = i.toString();
+            keyStr.innerText = NoteManager.getKey(i);
             key.appendChild(keyStr);
 
+
+            // calculate the position of the div
             let noteOctave = Math.floor(i / 12) - Math.floor(lowest / 12);
             let offset = offsets[i % 12] + noteOctave * 7;
             if (Number.isInteger(offset) === true){  // Is white key
@@ -59,6 +64,7 @@ class KeyboardElement{
                 key.style.left = `${offset * keyWidth * 100 + keyWidth * 100 / 4}%`;    // + 1/4 width
             }
 
+            // Assign the html property to the div
             key.id = i.toString();
             key.setAttribute('touch-action', 'none');
 
@@ -69,57 +75,67 @@ class KeyboardElement{
     }
 
     _bindKeyEvents(key){
+        const chordSteps = this._getChordSteps();
 
-        key.addEventListener('pointerup', (e) => {
-            // Get the chord type
-            const chordTypeFrm = document.getElementById("chord_type_frm");
-            let chordType = "major";
-            for (let i = 0; i < chordTypeFrm.length; i++) {
-                if (chordTypeFrm[i].checked) {
-                    chordType = chordTypeFrm[i].value;
-                }
+        key.addEventListener('pointerdown', (e) => {
+            this._showChord(chordSteps, e.target.id)
+        });
+
+        document.addEventListener('keydown', (e) => {
+            const noteId = "1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./".indexOf(e.key);
+            if (noteId !== -1){
+                this._showChord(chordSteps, noteId)
             }
-
-            // Change the previous keys' color back to the original color
-            for (let i = 0; i < this._prevKeys.length; i++) {
-                const prevDiv = document.getElementById(this._prevKeys[i]);
-                prevDiv.style.removeProperty("background-color");
-            }
-
-            const chordSteps = chords[chordType];   // Get half steps of the chord
-
-            let chordStr = "";
-
-            // Change color of keys of the chord
-            for (let i = 0; i < chordSteps.length; i ++) {
-                const noteId = parseInt(e.target.id) + chordSteps[i];
-                chordStr += ` ${this._getNote(noteId)}`;
-                const noteDiv = document.getElementById(`${noteId}`);
-
-                if (noteDiv !== null) { // noteDiv in bound
-                    noteDiv.style.backgroundColor = "#f7d794";  // Change the color to yellow
-                    this._prevKeys.push(noteId);    // Save the key as a previous key
-                } else {
-                    console.log(`${noteId} out of bound`);
-                }
-            }
-
-            document.getElementById("chord").innerText = chordStr;  // Display the notes in the chord in 'chord' p
         })
     }
 
     /**
-     * Get the scientific notation of note based on id
-     * @param id: Integer
+     * Get the chord steps of the currently selected chord type
+     * @returns {String}
      * @private
-     * @return {String}
      */
-    _getNote(id){
-        let note = id % 12;
-        const chromatic = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
-        note = chromatic[note];
-        const octave = Math.floor(id / 12) + 2;
-        return `${note}${octave}`;
+    _getChordSteps(){
+        // Get the chord type
+        const chordTypeFrm = document.getElementById("chord_type_frm");
+        let chordType = "major";
+        for (let i = 0; i < chordTypeFrm.length; i++) {
+            if (chordTypeFrm[i].checked) {
+                chordType = chordTypeFrm[i].value;
+            }
+        }
+        return chords[chordType];
+    }
+
+    /**
+     * Show the chord by displaying the chord string and highlight the keys in the chord
+     * @param chordSteps
+     * @param id of selected key
+     * @private
+     */
+    _showChord(chordSteps, id){
+        // Change the previous keys' color back to the original color
+        for (let i = 0; i < this._prevKeys.length; i++) {
+            const prevDiv = document.getElementById(this._prevKeys[i]);
+            prevDiv.style.removeProperty("background-color");
+        }
+
+        let chordStr = "";
+
+        // Change color of keys of the chord
+        for (let i = 0; i < chordSteps.length; i ++) {
+            const noteId = parseInt(id) + chordSteps[i];
+            chordStr += ` ${NoteManager.getNote(noteId)}`;
+            const noteDiv = document.getElementById(`${noteId}`);
+
+            if (noteDiv !== null) { // noteDiv in bound
+                noteDiv.style.backgroundColor = "#f7d794";  // Change the color to yellow
+                this._prevKeys.push(noteId);    // Save the key as a previous key
+            } else {
+                console.log(`${noteId} out of bound`);
+            }
+        }
+
+        document.getElementById("chord").innerText = chordStr;  // Display the notes in the chord in 'chord' p
     }
 }
 
---
+++ app/src/keyboard/NoteManager.mjs
@@ -0,0 +1,59 @@
+const chords = {
+    "major": [0, 4, 7],
+    "minor": [0, 3, 7],
+    "diminished": [0, 3, 6]
+};
+
+class NoteManager{
+    /**
+     * Get the scientific notation of note based on id
+     * @param id: Integer
+     * @private
+     * @return {String}
+     */
+    static getNote(id){
+        let note = id % 12;
+        const chromatic = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+        note = chromatic[note];
+        const octave = Math.floor(id / 12) + 1;
+        return `${note}${octave}`;
+    }
+
+    /**
+     * Get the key on the keyboard to trigger the sound of the id
+     * @param id
+     */
+    static getKey(id) {
+        return "1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./".charAt(id)
+    }
+
+    static getChordSteps(){
+        // Get the chord type
+        const chordTypeFrm = document.getElementById("chord_type_frm");
+        let chordType = "major";
+        for (let i = 0; i < chordTypeFrm.length; i++) {
+            if (chordTypeFrm[i].checked) {
+                chordType = chordTypeFrm[i].value;
+            }
+        }
+        return chords[chordType];
+    }
+
+    /**
+     * Get the list of notes in the selected chord
+     * @param id
+     * @returns {Array}
+     */
+    static getChordList(id){
+        const steps = NoteManager.getChordSteps();
+        let notes = [];
+        for (let i = 0; i < steps.length; i++){
+            const curId = parseInt(id) + steps[i];
+            const curNote = NoteManager.getNote(`${curId}`);
+            notes.push(curNote);
+        }
+        return notes
+    }
+}
+
+export {NoteManager}
