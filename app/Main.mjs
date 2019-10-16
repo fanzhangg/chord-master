0fd314ff26bbab820538273a41c3b4cb2caa8d5a
fanzhangg <vanadiumzhang@gmail.com>
2019-10-15 23:30:32 -0500

    Delete useless codes

--- app/Main.mjs
+++ app/Main.mjs
@@ -2,30 +2,11 @@ import {Keyboard} from "./src/keyboard/Keyboard.mjs";
 // import {Tone} from "tone";
 // import Tone from "tone";
 
-
-/**
- * Initialize the page
- */
-
-class Initializer {
-    constructor() {
-        Initializer.setDefaultChordType();
-    }
-    static setDefaultChordType() {
-        const chordTypeFrm = document.getElementById("chord_type_frm");
-        chordTypeFrm[0].checked = true; // Set major to be checked by default
-    }
-}
-
+// Add a container
 const container = document.createElement('div');
 container.id = 'container';
 document.body.appendChild(container);
 
 const keyboard = new Keyboard(container);
-
-// Set default chord type
-const chordTypeFrm = document.getElementById("chord_type_frm");
-chordTypeFrm[0].checked = true; // Set major to be checked by default
-
 
 
