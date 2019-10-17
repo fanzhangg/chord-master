5d1d0e488080be0c040fac0932b7263033068bbb
fanzhangg <vanadiumzhang@gmail.com>
2019-10-16 21:18:17 -0500

    FIX: delete ChordTypeSelector related import, and methods

--- app/Main.mjs
+++ app/Main.mjs
@@ -1,15 +1,9 @@
 import {Keyboard} from "./src/keyboard/Keyboard.mjs";
-import {ChordTypeSelector} from "./src/chordTypeSelector.mjs";
 
 // Add keyboard view
 const keyboardContainer = document.createElement('div');
 keyboardContainer.id = 'keyboard-container';
 document.body.appendChild(keyboardContainer);
 
-const keyboard = new Keyboard(keyboardContainer);
+new Keyboard(keyboardContainer);
 
-// // Add chord type selector view
-// const chordTypeContainer = document.createElement('div');
-// chordTypeContainer.id = 'chord-type-selector-container';
-// document.body.appendChild(chordTypeContainer);
-// new ChordTypeSelector(chordTypeContainer);
