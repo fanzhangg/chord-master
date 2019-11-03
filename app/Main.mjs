ce9b1abf5fbaa7a5975da65fac36f1b0619569f7
fanzhangg <vanadiumzhang@gmail.com>
2019-11-03 12:58:30 -0600

    WIP: add a loading page before the audio is loaded

--- app/Main.mjs
+++ app/Main.mjs
@@ -1,16 +1,28 @@
+/* global d3 */
 import {Keyboard} from "./src/keyboard/Keyboard.mjs";
 import {ChordTypeSelector} from "./src/ChordTypeSelector.mjs";
 
-// Add keyboard view
-const keyboardContainer = document.createElement('div');
-keyboardContainer.id = 'keyboard-container';
-document.body.appendChild(keyboardContainer);
 
-const keyboard = new Keyboard(keyboardContainer);
+const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+};
+
+const loader = d3.select("body").append("div")
+    .attr("id", "loader");
+
+sleep(1000).then(() => {
+    // Add keyboard view
+    d3.select("#loader").remove();
+    const keyboardContainer = document.createElement('div');
+    keyboardContainer.id = 'keyboard-container';
+    document.body.appendChild(keyboardContainer);
+
+    const keyboard = new Keyboard(keyboardContainer);
 
 // Add chord type selector view
-const chordTypeContainer = document.createElement('div');
-chordTypeContainer.id = 'chord_type_frm';
-chordTypeContainer.classList.add("container");
-document.body.insertBefore(chordTypeContainer, document.body.firstChild);    // Insert as the first child
-new ChordTypeSelector(chordTypeContainer);
+    const chordTypeContainer = document.createElement('div');
+    chordTypeContainer.id = 'chord_type_frm';
+    chordTypeContainer.classList.add("container");
+    document.body.insertBefore(chordTypeContainer, document.body.firstChild);    // Insert as the first child
+    new ChordTypeSelector(chordTypeContainer);
+});
