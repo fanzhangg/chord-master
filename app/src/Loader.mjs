9fa25f727b6747e7ec9738171897e54674c60133
fanzhangg <vanadiumzhang@gmail.com>
2019-11-05 10:34:20 -0600

    CHANGE: display the loading screen until the audio is loaded

--- app/src/Loader.mjs
+++ app/src/Loader.mjs
@@ -1,5 +1,7 @@
 /* global d3 */
 import {d3} from "third_party/d3.js"
+import {Tone} from "third_party/Tone.js"
+import {} from "third_party/StartAudioContext.js"
 
 class Loader {
     constructor(container){
@@ -7,7 +9,16 @@ class Loader {
         loader.id = "loader";
         container.appendChild(loader);
 
+        StartAudioContext(Tone.context, loader);
+        // StartAudioContext starts the Web Audio API's AudioContext on an explicit user action.
 
+        this.loaded = false;
+
+        Buffer.on("load", () => {
+            this.loaded = true;
+            document.getElementById("loader").remove();
+            // TODO: add the piano interface when loading is done
+        })
     }
 }
 
