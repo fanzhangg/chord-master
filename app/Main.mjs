88c37b89afc1ef38fb4fad1a58dc970a5046cb62
fanzhangg <vanadiumzhang@gmail.com>
2019-11-08 10:57:30 -0600

    CHANGE: append setting toolbar after the navbar

--- app/Main.mjs
+++ app/Main.mjs
@@ -1,6 +1,14 @@
 import {Keyboard} from "./src/keyboard/Keyboard.mjs";
 import {ChordTypeSelector} from "./src/ChordTypeSelector.mjs";
 
+
+// Add chord type selector view
+const chordTypeContainer = document.createElement('div');
+chordTypeContainer.id = 'chord_type_frm';
+chordTypeContainer.classList.add("container");
+document.body.appendChild(chordTypeContainer, document.body.firstChild);    // Insert as the first child
+new ChordTypeSelector(chordTypeContainer);
+
 // Add keyboard view
 const keyboardContainer = document.createElement('div');
 keyboardContainer.id = 'keyboard-container';
@@ -8,9 +16,4 @@ document.body.appendChild(keyboardContainer);
 
 const keyboard = new Keyboard(keyboardContainer);
 
-// Add chord type selector view
-const chordTypeContainer = document.createElement('div');
-chordTypeContainer.id = 'chord_type_frm';
-chordTypeContainer.classList.add("container");
-document.body.insertBefore(chordTypeContainer, document.body.firstChild);    // Insert as the first child
-new ChordTypeSelector(chordTypeContainer);
+
