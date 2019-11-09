2c96dfc767533ab8f4f501407d47e8501ff1a42b
fanzhangg <vanadiumzhang@gmail.com>
2019-11-09 13:33:31 -0600

    FIX: use d3 to create a toolbar to contain the chord type and inversion btns

--- app/Main.mjs
+++ app/Main.mjs
@@ -1,13 +1,14 @@
 import {Keyboard} from "./src/keyboard/Keyboard.mjs";
 import {ChordTypeSelector} from "./src/ChordTypeSelector.mjs";
+import {ChordSettingToolbar} from "./src/settingToolbar/ChordSettingToolbar.mjs";
 
 
 // Add chord type selector view
-const chordTypeContainer = document.createElement('div');
-chordTypeContainer.id = 'chord_type_frm';
-chordTypeContainer.classList.add("container");
-document.body.appendChild(chordTypeContainer, document.body.firstChild);    // Insert as the first child
-new ChordTypeSelector(chordTypeContainer);
+// const chordTypeContainer = document.createElement('div');
+// chordTypeContainer.id = 'chord_type_frm';
+// chordTypeContainer.classList.add("container");
+// document.body.appendChild(chordTypeContainer, document.body.firstChild);    // Insert as the first child
+new ChordSettingToolbar();
 
 // Add keyboard view
 const keyboardContainer = document.createElement('div');
--- app/src/settingToolbar/ChordSettingToolbar.mjs
+++ app/src/settingToolbar/ChordSettingToolbar.mjs
@@ -4,11 +4,17 @@ import {ChordTypeBtnGroup} from "./ChordTypesBtnGroup.mjs";
 import {InversionTypeBtnGroup} from "./InversionTypeBtnGroup.mjs";
 
 class ChordSettingToolbar{
-    constructor(container){
-        const toolbar = d3.select(container).append("div")
-            .attr("class", "btn-toolbar")
-            .attr("role", "toolbar")
-            .attr("aria-label", "Toolbar to set chord");
+    constructor(){
+        const toolbar = d3.select("body").append("div") // Append container
+            .attr("class", "container-fluid bg-dark")
+                .append("div")  // Append row div
+                    .attr("class", "row")
+                        .append("div")
+                            .attr("class", "p-2")
+                                .append("div")  // Append btn-toolbar
+                                    .attr("class", "btn-toolbar")
+                                    .attr("role", "toolbar")
+                                    .attr("aria-label", "Toolbar to set chord");
 
         const chordTypeBtnGroup = new ChordTypeBtnGroup(toolbar.node());
         const inversionTypeBtnGroup = new InversionTypeBtnGroup(toolbar.node());
