4679bd091b9224eaca4e1e668cce4c1451a38aea
fanzhangg <vanadiumzhang@gmail.com>
2019-11-10 20:58:14 -0600

    CHANGE: show the loader when the buffer is loading, show the percentage when the buffer is on progress

--- app/src/Loader.mjs
+++ app/src/Loader.mjs
@@ -1,23 +1,33 @@
 /* global d3 */
-import {d3} from "third_party/d3.js"
-import {Tone} from "third_party/Tone.js"
-import {} from "third_party/StartAudioContext.js"
+/* global Tone */
+import {} from "./third_party/d3.js"
+import "./third_party/Tone.js"
+// import "./third_party/StartAudioContext.js"
 
 class Loader {
     constructor(container){
         const loader = document.createElement("div");
         loader.id = "loader";
-        container.appendChild(loader);
+        document.body.appendChild(loader);
 
-        StartAudioContext(Tone.context, loader);
+        const progress = document.createElement("p");
+        document.body.appendChild(progress);
+
+        // StartAudioContext(Tone.context, loader);
         // StartAudioContext starts the Web Audio API's AudioContext on an explicit user action.
 
         this.loaded = false;
 
-        Buffer.on("load", () => {
+        Tone.Buffer.on("load", () => {
             this.loaded = true;
             document.getElementById("loader").remove();
+            const startBtn = document.createElement("button");
+            document.body.appendChild(startBtn);
             // TODO: add the piano interface when loading is done
+        });
+
+        Tone.Buffer.on("progress", (prog) => {
+            progress.innerText = `${(prog * 100).toFixed(2)}%`
         })
     }
 }
