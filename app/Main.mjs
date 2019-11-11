f35abafd2edb9b3efdce17d640027a9ee882b16d
fanzhangg <vanadiumzhang@gmail.com>
2019-11-10 22:30:17 -0600

    WIP: load the piano sound, release the sound after releasing the mouse

--- app/Main.mjs
+++ app/Main.mjs
@@ -1,28 +1,51 @@
 /* global d3 */
 import {Keyboard} from "./src/keyboard/Keyboard.mjs";
 import {ChordTypeSelector} from "./src/ChordTypeSelector.mjs";
+import {Sound} from "./src/sound/Sound.mjs";
+import {Loader} from "./src/Loader.mjs";
 
+// const keyboardContainer = document.createElement('div');
+// keyboardContainer.id = 'keyboard-container';
+// document.body.appendChild(keyboardContainer);
+//
+// const keyboard = new Keyboard(keyboardContainer);
 
-const sleep = (milliseconds) => {
-    return new Promise(resolve => setTimeout(resolve, milliseconds))
-};
+const sound = new Sound();
+sound.load();
+const load = new Loader();
 
-const loader = d3.select("body").append("div")
-    .attr("id", "loader");
-
-sleep(1000).then(() => {
-    // Add keyboard view
-    d3.select("#loader").remove();
-    const keyboardContainer = document.createElement('div');
-    keyboardContainer.id = 'keyboard-container';
-    document.body.appendChild(keyboardContainer);
-
-    const keyboard = new Keyboard(keyboardContainer);
-
-// Add chord type selector view
-    const chordTypeContainer = document.createElement('div');
-    chordTypeContainer.id = 'chord_type_frm';
-    chordTypeContainer.classList.add("container");
-    document.body.insertBefore(chordTypeContainer, document.body.firstChild);    // Insert as the first child
-    new ChordTypeSelector(chordTypeContainer);
+window.addEventListener("pointerdown", function (){
+    sound.keyDown("56");
+    console.log("Trigger 56")
 });
+
+window.addEventListener("pointerup", function (){
+    sound.keyUp("56");
+    console.log("Release 56")
+});
+
+
+
+// const sleep = (milliseconds) => {
+//     return new Promise(resolve => setTimeout(resolve, milliseconds))
+// };
+//
+// const loader = d3.select("body").append("div")
+//     .attr("id", "loader");
+//
+// sleep(1000).then(() => {
+//     // Add keyboard view
+//     d3.select("#loader").remove();
+//     const keyboardContainer = document.createElement('div');
+//     keyboardContainer.id = 'keyboard-container';
+//     document.body.appendChild(keyboardContainer);
+//
+//     const keyboard = new Keyboard(keyboardContainer);
+//
+// // Add chord type selector view
+//     const chordTypeContainer = document.createElement('div');
+//     chordTypeContainer.id = 'chord_type_frm';
+//     chordTypeContainer.classList.add("container");
+//     document.body.insertBefore(chordTypeContainer, document.body.firstChild);    // Insert as the first child
+//     new ChordTypeSelector(chordTypeContainer);
+// });
