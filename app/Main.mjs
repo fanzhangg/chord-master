ff660c9125a577ce1caba0465a69d3e147b108d7
fanzhangg <vanadiumzhang@gmail.com>
2019-11-11 09:23:57 -0600

    ADD: a loading screen before the audio is loaded

--- app/Main.mjs
+++ app/Main.mjs
@@ -30,22 +30,21 @@ window.addEventListener("pointerup", function (){
 //     return new Promise(resolve => setTimeout(resolve, milliseconds))
 // };
 //
-// const loader = d3.select("body").append("div")
-//     .attr("id", "loader");
-//
-// sleep(1000).then(() => {
-//     // Add keyboard view
-//     d3.select("#loader").remove();
-//     const keyboardContainer = document.createElement('div');
-//     keyboardContainer.id = 'keyboard-container';
-//     document.body.appendChild(keyboardContainer);
-//
-//     const keyboard = new Keyboard(keyboardContainer);
-//
-// // Add chord type selector view
-//     const chordTypeContainer = document.createElement('div');
-//     chordTypeContainer.id = 'chord_type_frm';
-//     chordTypeContainer.classList.add("container");
-//     document.body.insertBefore(chordTypeContainer, document.body.firstChild);    // Insert as the first child
-//     new ChordTypeSelector(chordTypeContainer);
-// });
+const loader = d3.select("body").append("div")
+    .attr("id", "loader");
+
+// Add keyboard view
+d3.select("#loader").remove();
+const keyboardContainer = document.createElement('div');
+keyboardContainer.id = 'keyboard-container';
+document.body.appendChild(keyboardContainer);
+
+const keyboard = new Keyboard(keyboardContainer);
+
+// Add chord type selector view
+const chordTypeContainer = document.createElement('div');
+chordTypeContainer.id = 'chord_type_frm';
+chordTypeContainer.classList.add("container");
+document.body.insertBefore(chordTypeContainer, document.body.firstChild);    // Insert as the first child
+new ChordTypeSelector(chordTypeContainer);
+
--- app/src/Loader.mjs
+++ app/src/Loader.mjs
@@ -5,13 +5,14 @@ import "./third_party/Tone.js"
 // import "./third_party/StartAudioContext.js"
 
 class Loader {
-    constructor(container){
+    constructor(){
         const loader = document.createElement("div");
         loader.id = "loader";
         document.body.appendChild(loader);
 
-        const progress = document.createElement("p");
-        document.body.appendChild(progress);
+        const mask = document.createElement("div");
+        mask.id = "mask";
+        document.body.appendChild(mask);
 
         // StartAudioContext(Tone.context, loader);
         // StartAudioContext starts the Web Audio API's AudioContext on an explicit user action.
@@ -21,13 +22,12 @@ class Loader {
         Tone.Buffer.on("load", () => {
             this.loaded = true;
             document.getElementById("loader").remove();
-            const startBtn = document.createElement("button");
-            document.body.appendChild(startBtn);
+            mask.remove();
             // TODO: add the piano interface when loading is done
         });
 
         Tone.Buffer.on("progress", (prog) => {
-            progress.innerText = `${(prog * 100).toFixed(2)}%`
+            // progress.innerText = `${(prog * 100).toFixed(2)}%`
         })
     }
 }
