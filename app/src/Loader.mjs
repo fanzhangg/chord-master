fc96cb099ba67b0c3d05b5faef1f701bbc164c3f
fanzhangg <vanadiumzhang@gmail.com>
2019-11-12 20:52:15 -0600

    DELETE: add a mask when loading, remove the mask after loading is finished

--- app/src/Loader.mjs
+++ app/src/Loader.mjs
@@ -1,8 +1,7 @@
 /* global d3 */
 /* global Tone */
-import {} from "./third_party/d3.js"
+import "./third_party/d3.js"
 import "./third_party/Tone.js"
-// import "./third_party/StartAudioContext.js"
 
 class Loader {
     constructor(){
@@ -10,9 +9,7 @@ class Loader {
         loader.id = "loader";
         document.body.appendChild(loader);
 
-        const mask = document.createElement("div");
-        mask.id = "mask";
-        document.body.appendChild(mask);
+        const mask = document.querySelector("#mask");
 
         // StartAudioContext(Tone.context, loader);
         // StartAudioContext starts the Web Audio API's AudioContext on an explicit user action.
@@ -24,12 +21,7 @@ class Loader {
             this.loaded = true;
             document.getElementById("loader").remove();
             mask.remove();
-            // TODO: add the piano interface when loading is done
         });
-
-        Tone.Buffer.on("progress", (prog) => {
-            // progress.innerText = `${(prog * 100).toFixed(2)}%`
-        })
     }
 }
 
