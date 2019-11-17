a0c9f7b43920e8ebacfe304c0c421ee718491b52
fanzhangg <vanadiumzhang@gmail.com>
2019-11-17 17:22:15 -0600

    ADD: ChordSettingToolbar, ChordTYpesBtnGroup, InversionTYpeBtnGroup

---
+++ src/ts/setting-toolbar/ChordSettingToolbar.ts
@@ -0,0 +1,25 @@
+import * as d3 from "d3";
+import {ChordTypeBtnGroup} from "./ChordTypesBtnGroup";
+import {InversionTypeBtnGroup} from "./InversionTypeBtnGroup";
+
+class ChordSettingToolbar{
+    constructor(){
+        const toolbar = d3.select("body").append("div") // Append container
+            .attr("class", "container-fluid bg-dark")
+                .append("div")  // Append row div
+                    .attr("class", "row")
+                        .append("div")
+                            .attr("class", "p-2")
+                                .append("div")  // Append btn-toolbar
+                                    .attr("class", "btn-toolbar")
+                                    .attr("id", "settingToolbar")
+                                    .attr("role", "toolbar")
+                                    .attr("aria-label", "Toolbar to set chord");
+
+        const toolbarEle = toolbar.node() as HTMLElement;
+        new ChordTypeBtnGroup(toolbarEle);
+        new InversionTypeBtnGroup(toolbarEle);
+    }
+}
+
+export {ChordSettingToolbar}
---
+++ src/ts/setting-toolbar/InversionTypeBtnGroup.ts
@@ -0,0 +1,65 @@
+import * as $ from "jquery";
+import {Chord} from "../Chord";
+
+class InversionTypeBtnGroup{
+    constructor(container: HTMLElement) {
+        this._renderView(container); // renders the inversion dropdown
+        this._keyDown(); // adds dropdown logic
+    }
+
+    _renderView(container: HTMLElement) {
+
+        const dropdown = document.createElement("div"); // Inversion button group
+        dropdown.classList.add("dropdown", "ml-3");
+        dropdown.id = "inversionTypeDropdown";
+
+        const button = document.createElement("button");
+        button.classList.add("btn", "btn-secondary", "dropdown-toggle", "disabled");    // Initially set disabled
+        button.setAttribute("data-toggle", "dropdown");
+        button.id = "inversionDropdownBtn";
+        button.innerText = "None";
+
+        this._renderLabel(dropdown);
+
+        dropdown.appendChild(button);
+
+        const dropdownMenu = document.createElement("div");
+        dropdownMenu.classList.add("dropdown-menu");
+        dropdownMenu.classList.add("aria-labelledby", "dropdownMenuButton");
+        dropdownMenu.id = "inversionDropdownMenu";
+
+        dropdown.appendChild(dropdownMenu);
+
+        container.appendChild(dropdown);
+    }
+
+    _renderLabel(container: HTMLElement){
+        const label = document.createElement("span");
+        label.innerText = "Inversion:";
+        label.classList.add("text-muted", "btn");
+        label.setAttribute("disabled", "true");
+        container.appendChild(label);
+    }
+
+    _keyDown(){
+        $("#inversionDropdownMenu").on("click",e=>{
+            console.log("Item is clicked");
+            const item = $(e.target);
+            const inversionNum = parseInt($(item).attr("data-inversion-num") as string, 10);   // Get the number of the inversion
+            Chord.inversionNum = inversionNum;  // Assign inversion num
+            Chord.setHalfSteps();
+            console.log(`Chord.inversionNum: ${Chord.inversionNum}`);
+            const button = $("#inversionDropdownBtn");
+            $(button).text(item.text());
+            if (inversionNum === 0){    // Click on the None item
+                $(button.removeClass("active"));    // Deactivate the button
+            } else {
+                $(button).addClass("active");   // Activate the button
+            }
+
+        })
+    }
+}
+
+export {InversionTypeBtnGroup}
+
