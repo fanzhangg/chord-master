7514bdf41a261f7501d19c0171bf1599e57936e9
fanzhangg <vanadiumzhang@gmail.com>
2019-11-22 23:18:26 -0600

    DELETE: useless import

--- src/ts/setting-toolbar/ChordSettingToolbar.ts
+++ src/ts/setting-toolbar/ChordSettingToolbar.ts
@@ -1,7 +1,6 @@
 import * as d3 from "d3";
 import {ChordTypeBtnGroup} from "./ChordTypesBtnGroup";
 import {InversionTypeBtnGroup} from "./InversionTypeBtnGroup";
-import { ProgressionButtons } from "./ProgressionButtons";
 
 class ChordSettingToolbar{
     constructor(){
