05d8d2c32943de170a72a3e56384d5b2a3a8a04f
fanzhangg <vanadiumzhang@gmail.com>
2019-11-30 20:00:38 -0600

    FIX: change ChordFamilies to a static variable

--- src/ts/music-theory/Chord.ts
+++ src/ts/music-theory/Chord.ts
@@ -1,35 +1,36 @@
 import {Note} from "./Note"
 
-const chordFamilies: any = {
-    "Triads": {  // chord with 3 notes
-        "Major Triad": [0, 4, 7],
-        "Minor Triad": [0, 3, 7],
-        "Augmented Triad": [0, 4, 8],
-        "Diminished Triad": [0, 3, 6]
-    },
-    "Sevenths": {   // triads with a 7th added on
-        "Dominant Seventh": [0, 4, 7, 10],
-        "Major Seventh": [0, 4, 7, 11], // C E G B
-        "Minor Seventh": [0, 3, 7, 10],
-        "Diminished Seventh": [0, 3, 6, 9], // C E♭ G♭ B♭♭
-        "Half Diminished Seventh": [0, 3, 6, 10],
-        "Augmented Seventh": [0, 4, 8, 10], // C E G♯ B♭
-        "Augmented Major Seventh": [0, 4, 8, 11] // C E G♯ B
-    },
-    "Extended": {
-        "Dominant Ninth": [0, 4, 7, 10, 14],    // C E G B♭ D
-        "Dominant Seventh": [0, 4, 7, 10, 14, 16],  // C E G B♭ D F WRONG FIX!
-        "Dominant Thirteenth": [0, 4, 7, 10, 14, 16, 20] // C E G B♭ D F A
-    },
-    "Altered": {
-        "Seventh Augmented Fifth": [0, 4, 8, 10],   // C E G♯ B♭
-        "Seventh Minor Ninth": [0, 4, 7, 10, 13],   // C E G B♭ D♭
-        "Seventh Sharp Ninth": [0, 4, 7, 10, 15],  // C E G B♭ D♯
-        "Seventh Augmented Eleventh": [0, 4, 7, 10, 14, 18],   // C E G B♭ D F♯
-    }
-};
+
 
 class Chord {
+    static chordFamilies: any = {
+        "Triads": {  // chord with 3 notes
+            "Major Triad": [0, 4, 7],
+            "Minor Triad": [0, 3, 7],
+            "Augmented Triad": [0, 4, 8],
+            "Diminished Triad": [0, 3, 6]
+        },
+        "Sevenths": {   // triads with a 7th added on
+            "Dominant Seventh": [0, 4, 7, 10],
+            "Major Seventh": [0, 4, 7, 11], // C E G B
+            "Minor Seventh": [0, 3, 7, 10],
+            "Diminished Seventh": [0, 3, 6, 9], // C E♭ G♭ B♭♭
+            "Half Diminished Seventh": [0, 3, 6, 10],
+            "Augmented Seventh": [0, 4, 8, 10], // C E G♯ B♭
+            "Augmented Major Seventh": [0, 4, 8, 11] // C E G♯ B
+        },
+        "Extended": {
+            "Dominant Ninth": [0, 4, 7, 10, 14],    // C E G B♭ D
+            "Dominant Seventh": [0, 4, 7, 10, 14, 16],  // C E G B♭ D F WRONG FIX!
+            "Dominant Thirteenth": [0, 4, 7, 10, 14, 16, 20] // C E G B♭ D F A
+        },
+        "Altered": {
+            "Seventh Augmented Fifth": [0, 4, 8, 10],   // C E G♯ B♭
+            "Seventh Minor Ninth": [0, 4, 7, 10, 13],   // C E G B♭ D♭
+            "Seventh Sharp Ninth": [0, 4, 7, 10, 15],  // C E G B♭ D♯
+            "Seventh Augmented Eleventh": [0, 4, 7, 10, 14, 18],   // C E G B♭ D F♯
+        }
+    };
     public halfSteps = [0];
     public type: string; // Must have a type
     public family: string;    // Family is nullable if it is a  single note
@@ -47,7 +48,7 @@ class Chord {
      * Get an array of string of the note in the chord
      */
     public getNotes(): Array<string> {
-        const halfSteps = chordFamilies[this.family][this.type];
+        const halfSteps = Chord.chordFamilies[this.family][this.type];
         if (halfSteps == null){
             throw new Error("Cannot get the half steps");
         }
