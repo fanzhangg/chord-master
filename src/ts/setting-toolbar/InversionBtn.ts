84118c63b3bfe54fcef4c35e36bf1d646436826d
fanzhangg <vanadiumzhang@gmail.com>
2019-11-30 23:20:00 -0600

    Add a callback event to set the chord on the keyboard when the inversion changes

--- src/ts/setting-toolbar/InversionBtn.ts
+++ src/ts/setting-toolbar/InversionBtn.ts
@@ -9,15 +9,27 @@ export class InversionBtn{
         6: "Sixth"
     };
 
+    onSetInversion: Function;
+
     constructor() {
-        this.setInversionMenu(2); // renders the inversion dropdown
+        this._setInversionMenu(2); // renders the inversion dropdown
+
+        this.onSetInversion = function() {}
+    }
+
+    /**
+     * Reset inversion btn to None, and menu items based on the chord length
+     */
+    public reset(chordLen: number){
+        this._setInverionBtn("None");
+        this._setInversionMenu(chordLen - 1);
     }
 
     /**
      * Populate the items in the inversion menu from none to maxInversion
      * @param maxInversion
      */
-    setInversionMenu(maxInversion: number) {
+    private _setInversionMenu(maxInversion: number) {
         const menu = document.getElementById("chordInversionDropdownMenu")!;
         menu.innerHTML = ""; // Remove all items
         for (let i = 0; i <= maxInversion; i++){
@@ -27,19 +39,25 @@ export class InversionBtn{
             item.innerText = InversionBtn.inversionTypes[i];
             item.addEventListener("pointerup", e=> {
                 const btn = e.target as HTMLElement;
-                const inversion = btn.innerText;
-                this.setInversion(inversion);
+                const inversionName = btn.innerText;
+                const inversionNum = parseInt(btn.dataset.inversion!);
+                this._setInversion(inversionName, inversionNum);
             });
 
             menu.appendChild(item);
         }
     }
 
+    private _setInversion(inversionName: string, inversionNum: number){
+        this._setInverionBtn(inversionName);
+        this.onSetInversion(inversionNum);
+    }
+
     /**
      * Set the text in the inversion button
      * @param inversionName
      */
-    public setInversion(inversionName: string){
+    private _setInverionBtn(inversionName: string){
         const btn = document.getElementById("chordInversionBtn")!;
         btn.innerText = inversionName;
     }
