9f2c205a5f170427e0b36189baa66c0e0f851786
fanzhangg <vanadiumzhang@gmail.com>
2019-12-02 11:08:25 -0600

    Updates

--- ts.841fc46b.js
+++ ts.841fc46b.js
@@ -51724,7 +51724,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60300" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61840" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
